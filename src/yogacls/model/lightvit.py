96d1bd2eb9329acb9ce8e337c7d8a776246489a3
k-washi <bandad.kw@gmail.com>
2022-07-21 14:49:01 +0000

    add lightvit

---
+++ src/yogacls/model/lightvit.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from src.yogacls.LightViT.classification.lib.models.lightvit import LightViT
+
+
+def lightvit_tiny(num_classes=1000, **kwargs):
+    model_kwargs = dict(patch_size=8, embed_dims=[64, 128, 256], num_layers=[2, 6, 6],
+                        num_heads=[2, 4, 8, ], mlp_ratios=[8, 4, 4], num_tokens=8,
+                        num_classes=num_classes, **kwargs)
+    model = LightViT(**model_kwargs)
+    return model
+
+if __name__ == "__main__":
+    ckpt_path = "./pretrained/lightvit_tiny_78.7.ckpt"
+    from src.yogacls.model.util import load_weight
+    import torchsummary
+    import torch
+    
+    model = lightvit_tiny(num_classes=2)
+    model = load_weight(model, ckpt_path, head_ignore=True)
+    model.eval()
+    
+    x = torch.rand((1, 3, 224, 224))
+    y = model(x)
+    print(f"RES: {y}")
+    #torchsummary.summary(model, (3, 224, 224), device="cpu")
---
+++ src/yogacls/model/util.py
@@ -0,0 +1,31 @@
+from pyparsing import Optional
+import torch
+
+STATE_DICT_KEY = "state_dict"
+MODEL_HEAD_KEY = "head"
+
+def load_ckpt(ckpt_path:str):
+    _ckpt = torch.load(ckpt_path)
+    if isinstance(_ckpt, dict) and STATE_DICT_KEY in _ckpt:
+        _ckpt = _ckpt[STATE_DICT_KEY]
+    return _ckpt
+
+def load_weight(model:torch.nn.Module, ckpt_path:str, head_ignore=False):
+    ckpt = load_ckpt(ckpt_path)
+    model_dict = model.state_dict()
+    
+    pretrained_dict = {}
+    for k, v in ckpt.items():
+        if k in model_dict:
+            if head_ignore and MODEL_HEAD_KEY in k:
+                # headは無視
+                continue
+            pretrained_dict[k] = v
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict, strict=False)
+    return model
+    
+
+if __name__ == "__main__":
+    ckpt_path = "./pretrained/lightvit_tiny_78.7.ckpt"
+    load_ckpt(ckpt_path)
